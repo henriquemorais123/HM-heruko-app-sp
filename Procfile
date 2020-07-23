849761b13a8b259e17d6a43e2630c29eb63f0663
henriquemorais123 <henriquemorais166@gmail.com>
2020-07-23 14:53:35 -0300

    teste-v0

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run meu-primeiro-app.py
---
+++ meu-primeiro-app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    """simple demonstration of stress"""
+    st.title('Meu primeiro app')
+    st.subheader('Tudo ok ate aqui')
+
+if __name__ == '__main__':
+    main()
